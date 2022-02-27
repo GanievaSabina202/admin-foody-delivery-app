dce19e02bc4af93ad94c40aaaa357bf879db1db2
khayym <xeyyam.kerimov@gmail.com>
2022-02-27 23:39:44 +0400

    for vercel

--- src/features/offers/OffersContainer/OffersContainer.js
+++ src/features/offers/OffersContainer/OffersContainer.js
@@ -2,14 +2,12 @@ import React, { useEffect } from 'react';
 import { OffersTable } from '../OffersTable/OffersTable';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchOffers } from '../../../store/slices/offers/offersSlice';
-// import { getOffersDataUrl } from '../../../services/offers';
-// import { getDataOffers } from '../../../store/slices/offers/offersSlice';
 
 export const OffersContainer = () => {
 
     const dispatch = useDispatch();
     const { offers } = useSelector(state => state.offers)
-    
+
     const getAllData = () => {
         offers.length < 1 && dispatch(fetchOffers())
     }
