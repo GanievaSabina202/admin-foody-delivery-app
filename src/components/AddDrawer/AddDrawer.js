39a123f2963bc14e10aa6c0e3c9226a49835cb6c
khayym <xeyyam.kerimov@gmail.com>
2022-03-02 03:24:05 +0400

    fixing error vercel

--- src/components/AddDrawer/AddDrawer.js
+++ src/components/AddDrawer/AddDrawer.js
@@ -32,10 +32,6 @@ export const AddDrawer = ({ children }) => {
             >
 
                 <CustomDropzone />
-
-                {
-                    children
-                }
             </AddDrawerStyled>
         </>
     );
