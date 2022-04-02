5b965c0dd6bfd62e74cf4b568f3f65dfdc56ed32
khayym <xeyyam.kerimov@gmail.com>
2022-04-03 02:45:48 +0400

    login system

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { AnimatePresence } from 'framer-motion'
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { AppProvider } from './app/AppProvider'
+import { AuthContextProvider } from './context/AuthContext'
 import Category from './pages/category'
 import Dashboard from './pages/Dashboard'
-import Login from './pages/login'
 import Offers from './pages/offers'
 import Orders from './pages/orders'
 import Products from './pages/products'
@@ -12,21 +12,22 @@ import Restaurants from './pages/Restuarants'
 
 const App = () => {
   return (
-    <AppProvider>
-      <AnimatePresence>
-        <Switch>
-          <Route exact path="/" component={Dashboard} />
-          <Route exact path="/panel" component={Dashboard} />
-          <Route path="/panel/dashboard" component={Dashboard} />
-          <Route path="/panel/products" component={Products} />
-          <Route path="/panel/category" component={Category} />
-          <Route path="/panel/offers" component={Offers} />
-          <Route path="/panel/restaurants" component={Restaurants} />
-          <Route path="/panel/orders" component={Orders} />
-          <Route path="/login" component={Login} />
-        </Switch>
-      </AnimatePresence>
-    </AppProvider>
+    <AuthContextProvider>
+      <AppProvider>
+        <AnimatePresence>
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+            <Route exact path="/panel" component={Dashboard} />
+            <Route path="/panel/dashboard" component={Dashboard} />
+            <Route path="/panel/products" component={Products} />
+            <Route path="/panel/category" component={Category} />
+            <Route path="/panel/offers" component={Offers} />
+            <Route path="/panel/restaurants" component={Restaurants} />
+            <Route path="/panel/orders" component={Orders} />
+          </Switch>
+        </AnimatePresence>
+      </AppProvider>
+    </AuthContextProvider>
   )
 }
 
